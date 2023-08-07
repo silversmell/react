120635a274eb21d994664bde5bfe24f7783702e5
eun hyang <ssm04090@gmail.com>
2023-08-07 20:04:15 +0900

    시작문구

--- src/App.js
+++ src/App.js
@@ -15,17 +15,46 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 export default App;**/
-import Button from "./Button";
+/**import Button from "./Button";**/
 
-function App() {
+/**export default function App() {
+    let name = 'park';
+    let link = 'https://codingapple.com/unit/nextjs-page-layout/';
     return (
         <div>
-            <Button text={"Continue"} />
+            <h4 className="title">Apple fresh</h4>
+            <p className="title-sub">by dev kim</p>
+            <a href={link}>link</a>
         </div>
     );
+}**/
+import React, { useEffect } from 'react'
+import axios from 'axios'
+// import { response } from 'express'
+
+function LandingPage() {
+
+    useEffect(() => {
+        axios.get('/api/hello')
+            .then(response => console.log(response.data))
+    }, [])
+
+    return (
+        <div style={{
+            display: 'flex', justifyContent: 'center', alignItems: 'center',
+            width: '100%', height: '100vh'
+        }}>
+            <h2>start page</h2>
+        </div>
+    )
 }
 
-export default App;
+export default LandingPage
+
+
+
+
+
 
 
 
